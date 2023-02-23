1660272e93da765b4cc4a8fefdc665b977e10156
chloemay-1 <chloewillis605@gmail.com>
2023-02-23 01:43:14 +0000

    adding the opening lines to the game

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def create_ship():
+    return random.randint(1, 8), random.randint(A, G)
+
+def jls_extract_def(try_again):
+    return try_again == "Y"
+def play_again():
+    try_again = input("Want to try again? <Y> <N>")
+    if jls_extract_def(try_again):
+       play_game()
+    else:
+        print("See you next time, Goodbye!")
+        return
+
+print("Welcome to Battleship"
+    "\nThis is the game where you go against the computer. Try to sink all of the computers ships in the number of turns you have. Best of Luck\n")
